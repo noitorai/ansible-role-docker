c0cd498c668df56944498e0e2542a037afba3ebc
ISHII Masaru <noitorai555+contact@gmail.com>
2018-09-03 04:51:44 +0000

    remove workaround of Ubuntu 14.04(Trusty)

--- Vagrantfile.rb
+++ Vagrantfile.rb
@@ -69,7 +69,6 @@ Vagrant.configure("2") do |config|
   # SHELL
   config.vm.provision "shell", inline: <<-SHELL
     apt-get update
-    apt-get install -y python python-apt python-pip
-    pip install ndg-httpsclient
+    apt-get install -y python python-apt
   SHELL
 end
